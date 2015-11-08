92fc30650cc5924cd586f9101d5989720de2fb50
svnsaip <svnsaip@tamu.edu>
2015-11-07 18:22:50 -0600

    first commit

---
+++ alpha_beta.hpp
@@ -0,0 +1,7 @@
+#ifndef ALPHA_BETA_HPP
+#define ALPHA_BETA_HPP
+
+
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"alpha_beta.hpp"
+
+using namespace std;
+
+int main() {
+    cout<<"hi"<<endl;
+    return 0;
+}
