1c1ed6580cd1ca7c518c74987a6877718ab10a4b
svnsaip <svnsaip@tamu.edu>
2015-11-09 17:58:06 -0600

    alpha-beta

--- alpha_beta.hpp
+++ alpha_beta.hpp
@@ -8,26 +8,33 @@
 
 //algorithm taken from https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning
 template<typename Graph, typename Heuristic>
-int alphabeta(Graph &g, typename boost::graph_traits<Graph>::vertex_descriptor v, int depth, int alpha, int beta, bool player1, Heuristic heuristic)
+int alphabeta(const Graph &g, const typename boost::graph_traits<Graph>::vertex_descriptor vd, int depth, int alpha, int beta, bool player1, Heuristic heuristic)
 {
   typename boost::graph_traits<Graph>::out_edge_iterator out_i, out_end;
 
-  if(depth == 0 || out_degree(v, g) == 0)
-    return heuristic(v);
+  if(depth == 0 || out_degree(vd, g) == 0) {
+    return heuristic(vd);
+  }
 
   if(player1) {
     int v = INT_MIN;
-    for(boost::tie(out_i, out_end) = out_edges(v, g); out_i != out_end; ++out_i) {
-      v = MAX(v, alphabeta(g, target(*out_i, g), depth-1, alpha, beta, false, heuristic));
+    for(boost::tie(out_i, out_end) = boost::out_edges(vd, g); out_i != out_end; ++out_i) {
+        std::cout<<"number of children "<<out_end - out_i <<std::endl;
+        std::cout<<"true"<<std::endl;
+        std::cout<<target(*out_i,g)<<std::endl;
+      v = MAX(v, alphabeta(g, target(*out_i, g), depth-1, alpha, beta, !player1, heuristic));
       alpha = MAX(alpha, v);
       if(beta <= alpha)
         break;//beta cut-off
     }
     return v;
   } else {
-    v = INT_MAX;
-    for(boost::tie(out_i, out_end) = out_edges(v, g); out_i != out_end; ++out_i) {
-      v = MIN(v, alphabeta(g, source(*out_i, g), depth-1, alpha, beta, true, heuristic));
+    int v = INT_MAX;
+    for(boost::tie(out_i, out_end) = boost::out_edges(vd, g); out_i != out_end; ++out_i) {
+        std::cout<<"number of children "<<out_end - out_i <<std::endl;
+        std::cout<<"false"<<std::endl;
+        std::cout<<target(*out_i,g)<<std::endl;
+      v = MIN(v, alphabeta(g, target(*out_i, g), depth-1, alpha, beta, !player1, heuristic));
       beta = MIN(beta, v);
       if(beta<=alpha)
         break;//alpha cut-off
--- main.cpp
+++ main.cpp
@@ -9,16 +9,68 @@ using namespace std;
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS> Graph;
 typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
 
-void make_graph(Graph g)
+void make_graph(Graph& g)
 {
+    boost::add_edge(0,1,g);
+    boost::add_edge(0,2,g);
+    boost::add_edge(0,3,g);
+    boost::add_edge(1,4,g);
+    boost::add_edge(1,5,g);
+    boost::add_edge(2,6,g);
+    boost::add_edge(2,7,g);
+    boost::add_edge(3,8,g);
+    boost::add_edge(3,9,g);
+    boost::add_edge(4,10,g);
+    boost::add_edge(4,11,g);
+    boost::add_edge(5,12,g);
+    boost::add_edge(5,13,g);
+    boost::add_edge(6,14,g);
+    boost::add_edge(6,15,g);
+    boost::add_edge(7,16,g);
+    boost::add_edge(7,17,g);
+    boost::add_edge(8,18,g);
+    boost::add_edge(8,19,g);
+    boost::add_edge(9,20,g);
+    boost::add_edge(9,21,g);
 }
 
 int main() {
-  cout<<"hi"<<endl;
   Graph g(10);
   make_graph(g);
   Vertex first = *boost::vertices(g).first;
-  std::cout<<alphabeta(g, first, 4, INT_MIN, INT_MAX, true, [](auto v) {return 1;})<<std::endl;
+  std::cout<<alphabeta(g, 0, 10, INT_MIN, INT_MAX, true, [](auto v) {
+            switch(v) {
+                case 10: return 4;
+                         break;
+                case 11: return 6;
+                         break;
+                case 12: return 7;
+                         break;
+                case 13: return 9;
+                         break;
+                case 14: return 1;
+                         break;
+                case 15: return 2;
+                         break;
+                case 16: return 0;
+                         break;
+                case 17: return 1;
+                         break;
+                case 18: return 8;
+                         break;
+                case 19: return 1;
+                         break;
+                case 20: return 9;
+                         break;
+                case 21: return 2;
+                         break;
+                default:
+                         return 0;
+                         break;
+
+            }
+          
+        })<<std::endl;
 
   return 0;
 }
