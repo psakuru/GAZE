fff27b83114ccf4e441823d550cddad58ab6cbb9
Chaitanya Vadrevu <chaitanya973@gmail.com>
2015-11-26 14:39:17 -0600

    gametree, some compile errors

--- gaze/gametree.hpp
+++ gaze/gametree.hpp
@@ -1,47 +1,113 @@
 #ifndef GAMETREE_HPP
 #define GAMETREE_HPP
 
+#include<boost/graph/adjacency_list.hpp>
+#include<boost/graph/graph_traits.hpp>
+
 namespace gaze {
 
-template<typename state>
-class vertex {
-private:
-  vertex(state&);
-  vertex(state&&);//move constructor
+template<typename edge_iter, typename vertex>
+class viterator : public std::iterator<std::forward_iterator_tag, vertex>{
+  edge_iter it;
 public:
-  state& get_state();
-  pair<vertex_iterator, vertex_iterator> get_children();
-  size_t get_children_count();
-}
+  viterator(edge_iter it): it(it) {}
+  viterator& operator=(const viterator& rhs) { return *this; }
+  viterator& operator++() {++it; return *this;};
+  bool operator==(const viterator& rhs) { return it==rhs.it; }
+  bool operator!=(const viterator& rhs) { return it!=rhs.it; }
+  vertex& operator*() { return *it; }
+};
 
-template<typename state>
+struct edge {
+  int hii;
+  edge() = default;
+};
+
+template<typename game_tree>
+class vertex {
+public:
+  typedef typename game_tree::graph graph;
+  typedef typename game_tree::state state;
+  typedef vertex<game_tree> vertex_property;
+  typedef typename boost::graph_traits<graph>::out_edge_iterator graph_edge_it;
+  typedef typename gaze::viterator<graph_edge_it, vertex_property> vertex_iterator;
+
+  typedef typename boost::graph_traits<graph>::vertex_descriptor vertex_descriptor;
+
+  vertex() {}
+  vertex(state* st, vertex_descriptor vd, game_tree* gt): st(st), vd(vd), gt(gt), g(&gt->g) {}
+
+  state& get_state() { return *st; }
+
+  std::pair<vertex_iterator, vertex_iterator> get_children() {
+    if(!children_added){
+      auto container = st->get_children();
+      for(auto it=container.begin(); it!=container.end(); it++){
+        //vertex_property *nvert = new vertex_property(it, gt);
+        auto tvd = boost::add_vertex(*g);
+        (*g)[tvd] = *new vertex_property(&*it, tvd, gt);
+        boost::add_edge(vd, tvd, *g);
+      }
+    }
+    typename boost::graph_traits<graph>::out_edge_iterator begin_edge_it, end_edge_it;
+    boost::tie(begin_edge_it, end_edge_it) = boost::out_edges(vd, *g);
+
+    vertex_iterator& begin_vertex_it = *new vertex_iterator(begin_edge_it);
+    vertex_iterator& end_vertex_it = *new vertex_iterator(end_edge_it);
+
+    return std::make_pair(begin_vertex_it, end_vertex_it);
+  }
+
+  bool operator==(const vertex& vt) {
+    return vd==vt.vd;
+  }
+  size_t get_children_count();
+  //friend class viterator<vertex, graph>;
+private:
+  vertex_descriptor vd = -1;
+  state* st = nullptr;
+  game_tree* gt = nullptr;
+  graph* g = nullptr;
+  bool children_added = false;
+};
+
+template<typename game_state>
 class game_tree {
 public:
-  typedef typename vertex<state> vertex_descriptor;
+  typedef game_state state;
+  typedef vertex<game_tree> vertex_property;
+  typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
+                        vertex_property, edge> graph;
+  typedef typename boost::graph_traits<graph>::vertex_descriptor vertex_descriptor;
 
-  game_tree(state initialstate);
+  typedef typename vertex_property::vertex_iterator vertex_iterator;
+
+  /*game_tree(state& initialstate):g(1) {
+    cur_vertex = new vertex_property(initialstate, *this);
+  }*/
+  game_tree(state* st) {
+    auto vd = boost::add_vertex(g);
+    cur_vertex = new vertex_property(st, vd, this);
+    g[vd] = *cur_vertex;
+  }
   game_tree(const game_tree& otherTree);
 
   //to be called for all moves
-  void set_current_state(state committedstate);
+  void set_current_state(state committedstate) {
+  }
   //returns the previously committed vetex, used by algo
-  vertex& get_current_vertex();
+  vertex_property& get_current_vertex() {
+    return *cur_vertex;
+  }
   //returns the previously committed state, used by game designer
-  state& get_current_state();
+  state& get_current_state() {
+    return cur_vertex->get_state();
+  }
+  graph g;
+private:
+  vertex_property *cur_vertex;
 };
 
 }
 
 #endif
-
-
-
-state {
-}
-
-namespace {
-state& get_parent(state& curr, game_tree& gt){
-  boost::in_edges(curr, gt);
-  source(curr, gt);
-}
-}
---
+++ gaze/test/treetest.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include "../gametree.hpp"
+
+using namespace std;
+
+class state {
+  int nodeno;
+public:
+  state(int no): nodeno(no) {}
+  vector<state>& get_children() {
+    vector<state>& lst = *new vector<state>();
+    lst.push_back(nodeno*3);
+    lst.push_back(nodeno*3+1);
+    return lst;
+  }
+  int get_value();
+  void print() {
+    cout<<"node: "<<nodeno<<endl;
+  }
+};
+
+gaze::game_tree<state> *gt;
+void init() {
+  //gt = new gaze::game_tree<state>(*new state(0));
+  gt = new gaze::game_tree<state>(new state(0));
+  auto vertex = gt->get_current_vertex();
+  auto iter_pair = vertex.get_children();
+  gaze::game_tree<state>::vertex_iterator it=iter_pair.first;
+  gaze::game_tree<state>::vertex_iterator eit=iter_pair.second;
+  //it==eit;
+  //it++;
+  (*it).get_state().print();
+  //for(gaze::game_tree<state>::vertex_iterator it=iter_pair.first;it!=iter_pair.second;it++)
+   // (*it).get_state().print();
+  //for_each(iter_pair.first, iter_pair.second, [](auto st) { st.print(); });
+}
+int main()
+{
+  init();
+}
